590b6ca66013776514c366b179dd9788cced7f5e
Nicola Kaleta <nic00la.kaleta@gmail.com>
2024-01-20 23:40:46 +0100

    Added start and end Points ✅👆

--- ShimmerEffect/ShimmerView.swift
+++ ShimmerEffect/ShimmerView.swift
@@ -9,12 +9,22 @@ import SwiftUI
 
 struct ShimmerView: View {
     
+    @State private var startPoint: UnitPoint = .init(x: -1.8, y: -1.2)
+    @State private var endPoint: UnitPoint = .init(x: 0, y: -0.2)
+    
+    
     private var gradientColors = [Color.gray.opacity(0.2), Color.white.opacity(0.2),
         Color.gray.opacity(0.2)
     ]
     
     var body: some View {
         LinearGradient(colors: gradientColors, startPoint: .leading, endPoint: .trailing)
+            .onAppear() {
+                withAnimation(.easeInOut(duration: 1).repeatForever(autoreverses:false)) {
+                    startPoint = .init(x: 1, y:1)
+                    endPoint = .init(x: 2.2, y: 2.2)
+                }
+            }
     }
 }
 
