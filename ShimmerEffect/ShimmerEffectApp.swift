5d5e46958c46c328b1497d01ec6f1998568fd77e
Nicola Kaleta <nic00la.kaleta@gmail.com>
2024-01-20 23:30:12 +0100

    Initial Commit

---
+++ ShimmerEffect/ShimmerEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShimmerEffectApp.swift
+//  ShimmerEffect
+//
+//  Created by Nicola Kaleta on 20/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct ShimmerEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
